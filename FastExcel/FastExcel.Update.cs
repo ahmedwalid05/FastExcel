3ed1384e305ec55eb95bba04a1189435a61781a8
mrjono <jonoclarnette@gmail.com>
2016-01-28 20:21:52 +1100

    Added comment to force build

--- FastExcel/FastExcel.Update.cs
+++ FastExcel/FastExcel.Update.cs
@@ -11,11 +11,21 @@ namespace FastExcel
 {
     public partial class FastExcel
     {
+        /// <summary>
+        /// Update the worksheet
+        /// </summary>
+        /// <param name="data">The worksheet</param>
+        /// <param name="sheetNumber">eg 1,2,4</param>
         public void Update(Worksheet data, int sheetNumber)
         {
             this.Update(data, sheetNumber, null);
         }
 
+        /// <summary>
+        /// Update the worksheet
+        /// </summary>
+        /// <param name="data">The worksheet</param>
+        /// <param name="sheetName">eg. Sheet1, Sheet2</param>
         public void Update(Worksheet data, string sheetName)
         {
             this.Update(data, null, sheetName);
